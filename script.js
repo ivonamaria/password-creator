0a1db7410a99955431791378e35a35e574db2ab0
Iwona Malkinska <iwonamalkinska@Iwonas-MacBook-Air.local>
2023-02-02 09:58:52 +0000

    added pseudocode

--- script.js
+++ script.js
@@ -88,25 +88,40 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// Function to prompt user for password options
-function getPasswordOptions() {
+// Create variables
 
-}
+function generatePassword(length, includeSpecial, includeNumeric, includeLowercase, includeUppercase) {
+  // Initialize the password variable
+  var password = "";
 
-// Function for getting a random element from an array
-function getRandom(arr) {
+  // Initialize an array to store all of the characters that will be included in the password
+  var characterTypes = [];
 
-}
+  // If includeSpecial is true, add the special characters array to characterTypes
+  if (includeSpecial) {
+    characterTypes = characterTypes.concat(specialCharacters);
+  }
 
-// Function to generate password with user input
-function generatePassword() {
+  // If includeNumeric is true, add the numeric characters array to characterTypes
+  if (includeNumeric) {
+    characterTypes = characterTypes.concat(numericCharacters);
+  }
 
-}
+  // If includeLowercase is true, add the lowercase characters array to characterTypes
+  if (includeLowercase) {
+    characterTypes = characterTypes.concat(lowerCasedCharacters);
+  }
 
-// Get references to the #generate element
+  // If includeUppercase is true, add the uppercase characters array to characterTypes
+  if (includeUppercase) {
+    characterTypes = characterTypes.concat(upperCasedCharacters);
+  }
+// Prompt user for password options using Function 
+
+// Get references to the generate button
 var generateBtn = document.querySelector('#generate');
 
-// Write password to the #password input
+// Write password to the password textarea
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
